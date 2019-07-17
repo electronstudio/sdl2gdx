bc3e4360dd63e8813b7ce68867e58598ec971c12
Richard Smith <mercurial@fantastic.me.uk>
2019-07-17 02:22:35 +0100

    allow to use with gdx lwjgl3 backend

---
+++ src/com/badlogic/gdx/controllers/lwjgl3/Lwjgl3ControllerManager.java
@@ -0,0 +1,7 @@
+package com.badlogic.gdx.controllers.lwjgl3;
+
+import uk.co.electronstudio.sdl2gdx.SDL2ControllerManager;
+
+public class Lwjgl3ControllerManager extends SDL2ControllerManager {
+}
+
